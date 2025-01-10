d18eb64f4eb424ffbb1aa1ee56ed0f2731b05bab
Edwin Serrano <edwseth.77@gmail.com>
2025-01-10 18:08:58 -0500

    Update DatosAutor.java
    Se simplifica el código

--- src/main/java/com/edwinserrano/ChallengeLiterAlura/model/DatosAutor.java
+++ src/main/java/com/edwinserrano/ChallengeLiterAlura/model/DatosAutor.java
@@ -1,26 +1,13 @@
 package com.edwinserrano.ChallengeLiterAlura.model;
 
-import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record DatosAutor(
-        @JsonAlias("name") String nombre,
-        @JsonAlias("birth_year") Integer fechaDeNacimiento,
-        @JsonAlias("death_year") Integer fechaDeMuerte
+public record AuthorData(
+    @JsonProperty("name") String name,
+    @JsonProperty("birth_year") int birth_year,
+    @JsonProperty("death_year") int death_year
 ) {
-    public Object getNombre() {
-        return nombre;
-    }
-    @Override
-    public String toString() {
-        // Comprueba si el nombre es nulo o vacío y muestra "Sin autor" si es necesario
-        String nombreAutor = nombre != null && !nombre.isEmpty() ? nombre : "Sin autor";
-        return String.format("%s\n        Año de nacimiento: %s\n        Año de muerte: %s",
-                nombre != null ? nombre : "Sin nombre",
-                fechaDeNacimiento != null ? fechaDeNacimiento : "Desconocido",
-                fechaDeMuerte != null ? fechaDeMuerte : "Desconocido");
-    }
-
 }
 
