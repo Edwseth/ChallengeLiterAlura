1ab464c7cc5389a5fd40b9f6bd416fc0f2a892c6
Edwin Serrano <edwseth.77@gmail.com>
2025-01-10 18:15:40 -0500

    Update DatosConsulta.java

--- src/main/java/com/edwinserrano/ChallengeLiterAlura/model/DatosConsulta.java
+++ src/main/java/com/edwinserrano/ChallengeLiterAlura/model/DatosConsulta.java
@@ -5,5 +5,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record DatosConsulta(List<DatosLibro> consultas) {
-    }
+public record ResultData(
+    List<BookData> results
+) {
+}
